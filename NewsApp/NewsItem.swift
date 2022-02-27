2f4c98e9e821c397a33f9d50c1ff4e4f23f9fb74
Blackfianit <divashinin@mail.ru>
2022-02-27 18:41:49 +0300

    Model setup

---
+++ NewsApp/NewsItem.swift
@@ -0,0 +1,22 @@
+//
+//  NewsItem.swift
+//  NewsApp
+//
+//  Created by Дмитрий Ивашинин on 27.02.2022.
+//
+
+struct NewsItem {
+    let title: String
+    let author: String
+    let description: String
+    let image: String
+    
+    
+    static func getNewsItem() -> [NewsItem] {
+        [
+            NewsItem(
+                title: "One", author: "", description: "", image: ""
+            )
+        ]
+    }
+}
